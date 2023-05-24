58eb56e4c2f957d2d4c26e062b3a5f152919849d
JungEunchong <21900673@handong.ac.kr>
2023-05-24 13:02:51 +0900

    Add: findeq.c

---
+++ findeq.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+int main(int argc, char* argv[])
+{
+    // Check invalid inputs
+    if (argc != 4) {
+
+    }
+
+    // Initiate thread and etc
+
+
+    // Run find function
+
+
+    // Finish program (free, ...)
+
+    return 0;
+}
